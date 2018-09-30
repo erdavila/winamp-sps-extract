2a790558867d228f52d085b7d206e71567534ef3
Eduardo R. D'Avila <erdavila@gmail.com>
2018-09-30 00:07:20 -0300

    Extract extraction code to a separate class

--- extractor/src/main/scala/waspse/Main.scala
+++ extractor/src/main/scala/waspse/Main.scala
@@ -1,44 +1,9 @@
 package waspse
 
-import java.io.File
-
-import waspse.sps.{Decoder, Parsers}
-import waspse.writers.{DecodedSPSWriter, ScalaWriter, TypeInferenceWriter}
-import waspse.typeInference.{TypeInferrer, TypeRequirementsAnalyzer}
-
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val spsFile = args(0)
-    val name = removeExtension(new File(spsFile).getName)
-    println(">>> " + name)
-
-    val sps = Decoder.decode(spsFile)
-    DecodedSPSWriter.write(sps, name)
-
-    val methods = sps.codes map Parsers.parse
-    ScalaWriter.write(methods, name, "parsed")
-
-    val normalizedMethods = methods map Normalizer.normalize
-    ScalaWriter.write(normalizedMethods, name, "untyped")
-
-    val varsTypesReqs = TypeRequirementsAnalyzer.analyzeIn(normalizedMethods)
-    val inferrer = new TypeInferrer(varsTypesReqs)
-    val inferredVarsTypes = inferrer.infer()
-    val defaultTypedVars = inferrer.defaultTypedVars(inferredVarsTypes.keySet)
-    TypeInferenceWriter.write(varsTypesReqs, inferredVarsTypes, defaultTypedVars, name)
-
-    val varsTypes = inferredVarsTypes ++ defaultTypedVars
-    val typedMethods = normalizedMethods map { new ExpressionTypeAdapter(varsTypes).adaptIn }
-    ScalaWriter.write(typedMethods, varsTypes, name, "typed")
-  }
-
-  private def removeExtension(fileName: String): String = {
-    val pos = fileName.indexOf('.')
-    if (pos >= 0) {
-      fileName.take(pos)
-    } else {
-      fileName
-    }
+    val steps = new Steps(spsPath = args(0))
+    steps.typedMethods
   }
 }
---
+++ extractor/src/main/scala/waspse/Steps.scala
@@ -0,0 +1,56 @@
+package waspse
+
+import java.io.File
+
+import waspse.sps.{DecodedSPS, Decoder, Parsers}
+import waspse.typeInference.{TypeInferrer, TypeRequirementsAnalyzer}
+import waspse.writers.{DecodedSPSWriter, ScalaWriter, TypeInferenceWriter}
+
+class Steps(spsPath: String) {
+  lazy val name: String = removeExtension(new File(spsPath).getName)
+
+  println(">>> " + name)
+
+  lazy val decodedSPS: DecodedSPS = {
+    val decoded = Decoder.decode(spsPath)
+    DecodedSPSWriter.write(decoded, name)
+    decoded
+  }
+
+  lazy val methods: Seq[BlockStatement] = {
+    val ms = decodedSPS.codes map Parsers.parse
+    ScalaWriter.write(ms, name, "parsed")
+    ms
+  }
+
+  lazy val normalizedMethods: Seq[Statement] = {
+    val normMs = methods map Normalizer.normalize
+    ScalaWriter.write(normMs, name, "untyped")
+    normMs
+  }
+
+  lazy val (inferredVarsTypes, defaultTypedVars) = {
+    val varsTypesReqs = TypeRequirementsAnalyzer.analyzeIn(normalizedMethods)
+    val inferrer = new TypeInferrer(varsTypesReqs)
+    val inferredVarsTypes = inferrer.infer()
+    val defaultTypedVars = inferrer.defaultTypedVars(inferredVarsTypes.keySet)
+    TypeInferenceWriter.write(varsTypesReqs, inferredVarsTypes, defaultTypedVars, name)
+    (inferredVarsTypes, defaultTypedVars)
+  }
+
+  lazy val typedMethods: Seq[Statement] = {
+    val varsTypes = inferredVarsTypes ++ defaultTypedVars
+    val typedMethods = normalizedMethods map { new ExpressionTypeAdapter(varsTypes).adaptIn }
+    ScalaWriter.write(typedMethods, varsTypes, name, "typed")
+    typedMethods
+  }
+
+  private def removeExtension(fileName: String): String = {
+    val pos = fileName.indexOf('.')
+    if (pos >= 0) {
+      fileName.take(pos)
+    } else {
+      fileName
+    }
+  }
+}
